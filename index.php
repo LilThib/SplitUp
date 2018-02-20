dad5277c4dae200453a262586503995712ed9753
Chocolat2 <gregory.preisig@gmail.com>
2018-02-20 11:34:24 +0100

    modified group creation

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
                 &nbsp;
             </div>
 <div class="justify-content-center col-md-12">
-<a href="DynamicForm"><button id="BeginBtn">Commencer</button></a>
+<a href="DynamicForm.php"><button id="BeginBtn">Commencer</button></a>
 </div>
             <!--<div class="col-md-12">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
