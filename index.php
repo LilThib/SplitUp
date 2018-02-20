ea1c6844ec141d96c01667c2cee2bed4d2438d12
Chocolat2 <gregory.preisig@gmail.com>
2018-02-20 09:33:50 +0100

    modified index.php

--- index.php
+++ index.php
@@ -18,35 +18,6 @@
         <link rel="stylesheet" href="css/main.css">
     </head>
     <body class="text-justify">
-
-        <!--<nav class="navbar navbar-default">
-            <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <!--        <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand titlel nav-link" href="index.php">Blog-IT</a>
-        <!--<a class="nav-link active" href="index.php">Home</a>
-        <a class="nav-link" href="post.php">Post</a>-->
-        <!--        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <!--        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="active" onmouseover="this.style.backgroundColor = '#111111';" onmouseout="this.style.backgroundColor = '#000000';"><a href="index.html">Accueil</a></li>
-                        <li onmouseover="this.style.backgroundColor = '#111111';" onmouseout="this.style.backgroundColor = '#000000';"><a href="detail.html">Detail</a></li>
-                        <li onmouseover="this.style.backgroundColor = '#111111';" onmouseout="this.style.backgroundColor = '#000000';"><a href="gallery.html">Gallerie</a></li>
-                        <li onmouseover="this.style.backgroundColor = '#111111';" onmouseout="this.style.backgroundColor = '#000000';"><a href="form.html">Formulaire</a></li>
-
-                    </ul>
-                </div>
-        <!-- /.navbar-collapse -->
-        <!--   </div>
-        <!-- /.container-fluid -->
         <!--</nav>-->
         <nav class="navbar navbar-toggleable-md sticky-top">
             <button class="navbar-toggler navbar-toggler-right" style="color: lightgrey;" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -61,19 +32,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="post.php">Post</a>
                     </li>
-                    <!--<li class="nav-item">
-                      <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown link
-                      </a>
-                      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                      </div>
-                    </li>-->
                 </ul>
             </div>
         </nav>
@@ -81,48 +39,36 @@
             <div class="col-md-12">
                 <!--<div class="jumbotron">-->
                 <p class="jumbotron">
-                    Fusce convallis, mauris imperdiet gravida bibendum, nisl
-                    turpis suscipit mauris, sed placerat ipsum urna sed risus.
-                    In convallis tellus a mauris. Curabitur non elit ut libero
-                    tristique sodales. Mauris a lacus. Donec mattis semper leo.
-                    In hac habitasse platea dictumst. Vivamus facilisis diam at
-                    odio. Mauris dictum, nisi eget consequat elementum, lacus
-                    ligula molestie metus, non feugiat orci magna ac sem. Donec
-                    turpis. Donec vitae metus. Morbi tristique neque eu mauris.
-                    Quisque gravida ipsum non sapien. Proin turpis lacus,
-                    scelerisque vitae, elementum at, lobortis ac, quam. Aliquam
-                    dictum eleifend risus. In hac habitasse platea dictumst.
-                    Etiam sit amet diam. Suspendisse odio. Suspendisse nunc.
-                    In semper bibendum libero.
+                    Site en cours de développement
                 </p>
 
             </div>
 
             <div class="row justify-content-center col-md-12">
-                <h3></h3>
+                <h3>Bienvenue sur le site Split-Up!</h3>
             </div>
 
             <div class="col-md-4 hidden-xs hidden-sm">
                 &nbsp;
             </div>
-            <div class="col-md-12">
+            <!--<div class="col-md-12">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </div><br/>
+            </div>--><br/>
 
 
             <div class="row">
                 <div class="col-sm-12 col-md-8">
-                    <p>
+                    <!--<p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <p>
+                    </p>-->
+                    <!--<p>
                         Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.
-                    </p>
+                    </p>-->
                 </div>
                 <div class="col-sm-12 col-md-4">
-                    <p>
+                    <!--<p>
                         Proin nonummy, lacus eget pulvinar lacinia, pede felis dignissim leo, vitae tristique magna lacus sit amet eros. Nullam ornare. Praesent odio ligula, dapibus sed, tincidunt eget, dictum ac, nibh. Nam quis lacus. Nunc eleifend molestie velit. Morbi lobortis quam eu velit. Donec euismod vestibulum massa. Donec non lectus. Aliquam commodo lacus sit amet nulla. Cras dignissim elit et augue. Nullam non diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In hac habitasse platea dictumst. Aenean vestibulum. Sed lobortis elit quis lectus. Nunc sed lacus at augue bibendum dapibus.
-                    </p>
+                    </p>-->
                 </div>
             </div>
         </div>
