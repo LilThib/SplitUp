1c3093c9ddd1cc8c3d8dba800aa0ebbdd444df73
JorgeJoGo <33324395+JorgeJoGo@users.noreply.github.com>
2018-02-27 10:52:45 +0100

    Delete algoGroup and put groupeCreation and groupePersonalisation in web html

--- groupCreation/groupPersonalisation.php
+++ groupCreation/groupPersonalisation.php
@@ -1,1 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Group Personalisation</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
