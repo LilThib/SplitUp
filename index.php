636bed4e1ec720781ec04bf5cde62434cfc7a539
LilThib <LilThib@users.noreply.github.com>
2018-03-06 10:48:31 +0100

    Mise à jours menu
    Ajout d'un lien sur la gestion des personne dans le menu

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
-<?php
-?>
-
 <html lang="en">
     <head>
         <title>Split-Up!</title>
+        
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
         <link href="./Bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -32,6 +29,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="post.php">Post</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="usersTable.php">Gestion des personnes</a>
+                    </li>
                 </ul>
             </div>
         </nav>
@@ -51,9 +51,9 @@
             <div class="col-md-4 hidden-xs hidden-sm">
                 &nbsp;
             </div>
-<div class="justify-content-center col-md-12">
-<a href="DynamicForm.php"><button id="BeginBtn">Commencer</button></a>
-</div>
+            <div class="justify-content-center col-md-12">
+                <a href="usersTable.php"><button id="BeginBtn">Commencer</button></a>
+            </div>
             <!--<div class="col-md-12">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </div>--><br/>
