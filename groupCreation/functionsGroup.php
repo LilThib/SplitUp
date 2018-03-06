0f5ba2920be01903348470243888249c9ae294a1
chocolat2 <gregory.preisig@gmail.com>
2018-03-06 09:34:36 +0100

    added groupByNbGroups in functionGroup.php

--- groupCreation/functionsGroup.php
+++ groupCreation/functionsGroup.php
@@ -1,1 +1,22 @@
 <?php
+function groupByNbGroup($listPeople, $nbGroups){
+  $nbPeople = count($listPeople);
+  $mod = $nbPeople % $nbGroups;
+  $nbPerson = $nbPeople-$mod
+  $nbPerson = $nbPerson/$nbGroups;
+  // $nbPerson : number of person per groups
+  // $nbPeople : numper of people in total
+  // $mod : number of person that cant be dispached equally between the groups
+
+  $j=$nbGroups;
+  for($i=0;$i<$nbPeople;$i++)
+  {
+    if ($j>=$nbGroups)
+    {
+      $j=0;
+    }
+    //put the person in a group
+    $group[j][i] = $listPerson[i];
+    $j++;
+  }
+}
